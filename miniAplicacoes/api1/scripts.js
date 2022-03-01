f4e29a2a2ead31fdf983418af95e0d93450f8e2e
lilicrst <lisandra.crisilva37@gmail.com>
2022-03-01 12:25:18 -0300

    começando a usar DOM

---
+++ miniAplicacoes/api1/scripts.js
@@ -0,0 +1,11 @@
+// adicionar id no elemento header
+const header = document.querySelector('header')
+header.setAttribute('id', 'header')
+
+// adicionar valor a um input
+const inputName = document.querySelector('input')
+inputName.value = "digite o seu nome"
+
+// adicionar texto
+const element = document.querySelector('h2')
+element.textContent += " | Seja bem vindo!"
