d0fb50e940f5ca848bdd0a3f86e2427838c95365
lilicrst <lisandra.crisilva37@gmail.com>
2022-03-03 12:13:52 -0300

    ...

--- miniAplicacoes/api1/scripts.js
+++ miniAplicacoes/api1/scripts.js
@@ -13,6 +13,28 @@ header.setAttribute('id', 'header')
 const inputName = document.querySelector('input')
 inputName.value = "digite o seu nome"
 
-// adicionar texto
+// adicionar (somar) texto
 const element = document.querySelector('h2')
 element.textContent += " | Seja bem vindo!"
+
+// criar e adicionar elementos
+const div1 = document.createElement('div')
+div1.innerText = 'Olá devs!'
+
+const body = document.querySelector('body') //selecionar o lugar onde o novo elemento será inserido
+body.insertBefore(div1, header.nextElementSibling)
+
+const headText = document.createElement('h5')
+headText.innerText = "Página de testes em HTML, CSS e JavaScrispt usando a DOM"
+body.prepend(headText) //adiciona ANTES de tudo no body
+
+const div2 = document.createElement('div')
+div2.innerText = "Vamos começar!"
+body.insertBefore(div2, div1.nextElementSibling) //recebe dois argumentos: o elemento a ser inserido & antes de qual ele vem
+
+const text2 = document.createElement('h4')
+text2.innerHTML = "Continua..."
+body.append(text2) //adiciona como filho DEPOIS de tudo no body
+
+
+
